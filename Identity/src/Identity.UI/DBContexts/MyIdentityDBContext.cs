eedd485c12658d2a4a111256b2396fbad7f12bf1
Samuel Colmenares <locoucla@hotmail.com>
2025-11-04 21:25:50 -0500

    feat: first commit

---
+++ Identity/src/Identity.UI/DBContexts/MyIdentityDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.UI.DBContexts;
+
+public class MyIdentityDBContext: IdentityDbContext
+{
+    public MyIdentityDBContext(DbContextOptions<MyIdentityDBContext> options) : base(options)
+    {
+        
+    }
+}
